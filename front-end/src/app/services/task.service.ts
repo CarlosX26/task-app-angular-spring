260942f2ff057971605058fcea9f05aac209e8d6
Carlos Junio <cjunior1@live.com>
2023-09-20 21:44:21 -0300

    feat: add toasts

--- front-end/src/app/services/task.service.ts
+++ front-end/src/app/services/task.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { HotToastService } from "@ngneat/hot-toast";
 import { BehaviorSubject, Observable } from "rxjs";
 
 export interface ITask {
@@ -16,14 +17,22 @@ export class TaskService {
   taskList: BehaviorSubject<ITask[]> = new BehaviorSubject([] as ITask[]);
   taskList$: Observable<ITask[]> = this.taskList.asObservable();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private toast: HotToastService) {}
+
+  onError() {
+    this.toast.error("Ops! Algo deu errado");
+  }
 
   createTask(title: string): void {
     this.http
       .post<ITask>(`${this.baseURL}/api/tasks`, { title })
       .subscribe((data) => {
         this.taskList.next([...this.taskList.value, data]);
-      });
+        this.toast.success("Tarefa criada com sucesso");
+      }),
+      () => {
+        this.onError();
+      };
   }
   getAllTasks(): void {
     this.http.get<ITask[]>(`${this.baseURL}/api/tasks`).subscribe((data) => {
@@ -31,9 +40,8 @@ export class TaskService {
     });
   }
   updateTask(id: number): void {
-    this.http
-      .patch<ITask>(`${this.baseURL}/api/tasks/${id}`, {})
-      .subscribe((data) => {
+    this.http.patch<ITask>(`${this.baseURL}/api/tasks/${id}`, {}).subscribe(
+      (data) => {
         const updateTaskList = this.taskList.value.map((task) => {
           if (task.id === id) {
             return data;
@@ -42,14 +50,25 @@ export class TaskService {
         });
 
         this.taskList.next(updateTaskList);
-      });
+      },
+      () => {
+        this.onError();
+      }
+    );
   }
   deleteTask(id: number): void {
-    this.http.delete<void>(`${this.baseURL}/api/tasks/${id}`).subscribe(() => {
-      const updateTaskList = this.taskList.value.filter(
-        (task) => task.id !== id
-      );
-      this.taskList.next(updateTaskList);
-    });
+    this.http.delete<void>(`${this.baseURL}/api/tasks/${id}`).subscribe(
+      () => {
+        const updateTaskList = this.taskList.value.filter(
+          (task) => task.id !== id
+        );
+        this.taskList.next(updateTaskList);
+
+        this.toast.success("Tarefa deletada com sucesso");
+      },
+      () => {
+        this.onError();
+      }
+    );
   }
 }
