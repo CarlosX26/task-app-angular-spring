6c90ef03f345050e15e1db830d0ef13de2e9eb3a
Carlos Junio <cjunior1@live.com>
2023-09-24 17:32:18 -0300

    refactor: service layer implementation

--- back-end/src/main/java/com/app/tasks/controller/TaskController.java
+++ back-end/src/main/java/com/app/tasks/controller/TaskController.java
@@ -2,7 +2,6 @@ package com.app.tasks.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,61 +14,41 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.tasks.entity.Task;
-import com.app.tasks.repository.TaskRepository;
+import com.app.tasks.service.TaskService;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
-import jakarta.persistence.EntityNotFoundException;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskController {
 
-  private final TaskRepository taskRepository;
+  private final TaskService taskService;
 
-  @Autowired
-  public TaskController(TaskRepository taskRepository) {
-    this.taskRepository = taskRepository;
+  public TaskController(TaskService taskService) {
+    this.taskService = taskService;
   }
 
   @GetMapping
   public List<Task> getAllTasks() {
-    return taskRepository.findAll();
+    return taskService.getAllTasks();
   }
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   public Task createTask(@RequestBody Task task) {
-    return taskRepository.save(task);
+    return taskService.createTask(task);
   }
 
   @PatchMapping("/{id}")
   public ResponseEntity<Task> markTaskAsCompleted(@PathVariable Long id) {
-    Task task = taskRepository.findById(id).orElse(null);
-
-    if (task != null) {
-
-      Boolean isCompleted = task.isCompleted();
-
-      task.setCompleted(!isCompleted);
-
-      taskRepository.save(task);
-
-      return ResponseEntity.ok(task);
-    } else {
-      throw new EntityNotFoundException("Task with ID " + id + " not found");
-    }
+    return taskService.updateTask(id);
   }
 
   @DeleteMapping("/{id}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
   public void deleteTask(@PathVariable Long id) {
-    if (taskRepository.existsById(id)) {
-      taskRepository.deleteById(id);
-    } else {
-      throw new EntityNotFoundException("Task with ID " + id + " not found");
-    }
+    taskService.deleteTask(id);
   }
 }
